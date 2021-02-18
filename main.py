2445c265d50742d2b2f2936a167fec352cf38db3
Joseph Jeong <jjeong@eab.com>
2021-02-18 14:55:23 -0500

    scrap one job post

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from bs4 import BeautifulSoup
+import requests
 
+
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+soup = BeautifulSoup(html_text, features='lxml')
+job = soup.find('li', class_='clearfix job-bx wht-shd-bx')
+company_name = job.find('h3', class_='joblist-comp-name').text.strip()
+skills = job.find('span', class_='srp-skills').text.strip()
+print(f'''
+Company Name: {company_name}
+Required Skills: {skills}
+''')
