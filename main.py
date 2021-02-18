64282eb2afebe9770e749ed848261bd113b3b2b8
Joseph Jeong <jjeong@eab.com>
2021-02-18 15:14:04 -0500

    refactor

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import requests
 
 html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup = BeautifulSoup(html_text, features='lxml')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
 
-for job in soup.find_all('li', class_='clearfix job-bx wht-shd-bx'):
+for job in jobs:
     company_name = job.find('h3', class_='joblist-comp-name').text.strip()
     skills = job.find('span', class_='srp-skills').text.strip()
     published_date = job.find('span', class_='sim-posted').text.strip().split('Posted')[-1]
